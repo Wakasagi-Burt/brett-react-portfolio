da72b7134b2d48714f5bed7ee6d9ebc5c56bd549
brett shulman <bshulman810@gmail.com>
2022-11-29 12:51:37 -0500

    added

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Footer from './components/footer';
+import Navbar from './components/navbar';
+import Aboutme from './components/aboutme';
+import Portfolio from './components/portfolio'
+
+
+
+
+
+
+
+
+export default function App() {
+  return (
+
+    <div> 
+      <Navbar/>
+      <Aboutme/>
+      <Footer/>
+      <Portfolio/>
+    </div>
+  );
+}
